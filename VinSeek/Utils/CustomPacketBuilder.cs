33c798e527828d3e13cd90e0814d66b769015dbb
Henry Munro <henry.mn2412@gmail.com>
2019-06-23 20:13:30 +0200

    Add reader for custom exported packets

--- VinSeek/Utils/CustomPacketBuilder.cs
+++ VinSeek/Utils/CustomPacketBuilder.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using VinSeek.Model;
+using Machina;
+using System.Text.RegularExpressions;
 
 namespace VinSeek.Utils
 {
@@ -35,6 +38,53 @@ namespace VinSeek.Utils
 
                 return output.ToArray();
             }
+        }
+
+        public static CapturedPacketInfo ReadPacket(byte[] buffer)
+        {
+            byte[] sourceIPBytes = new byte[15];
+            byte[] destIPBytes = new byte[15];
+            byte[] sourcePortBytes = new byte[5];
+            byte[] destPortBytes = new byte[5];
+            byte[] data = new byte[buffer.Length - 44];
+
+            Array.Copy(buffer, sourceIPBytes, 14);
+            var sourceIP = Encoding.ASCII.GetString(sourceIPBytes);
+            sourceIP = Regex.Replace(sourceIP, @"[^\u0020-\u007E]", string.Empty);
+
+            Array.Copy(buffer, 16, destIPBytes, 0, 15);
+            var destIP = Encoding.ASCII.GetString(destIPBytes);
+            destIP = Regex.Replace(destIP, @"[^\u0020-\u007E]", string.Empty);
+
+            Array.Copy(buffer, 32, sourcePortBytes, 0, 5);
+            var sourcePort = Encoding.ASCII.GetString(sourcePortBytes);
+            sourcePort = Regex.Replace(sourcePort, @"[^\u0020-\u007E]", string.Empty);
+
+            Array.Copy(buffer, 38, destPortBytes, 0, 5);
+            var destPort = Encoding.ASCII.GetString(destPortBytes);
+            sourcePort = Regex.Replace(sourcePort, @"[^\u0020-\u007E]", string.Empty);
+
+            Array.Copy(buffer, 43, data, 0, buffer.Length - 44);
+
+            string direction;
+
+            if (sourcePort == "27015")
+                direction = "Received";
+            else
+                direction = "Sent";
+            var pack = new CapturedPacketInfo
+            {
+                Direction = direction,
+                SourceIP = sourceIP.ToString(),
+                DestIP = destIP.ToString(),
+                SourcePort = sourcePort,
+                DestPort = destPort,
+                Protocol = IPProtocol.TCP,
+                DataLength = data.Length,
+                Data = data
+            };
+
+            return pack;
         }
     }
 }
